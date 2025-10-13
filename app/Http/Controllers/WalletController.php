39e819c566cacfc96f56a25ae33d7d7ba8527433
sourav6400 <imsourav6400@gmail.com>
2025-10-13 15:45:13 +0600

    Minor Update

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -931,7 +931,7 @@ class WalletController extends Controller
                 if($params['active_transaction_type'] == 'real')
                 {
                     $requestData = [
-                        "currency" => "ETH",
+                        "chain" => "ETH",
                         "to" => $params['receiverAddress'],
                         "fromPrivateKey" => $params['privateKey'],
                         "amount" => $params['amount'],
